ba6cb11e22a546018d3dac9fa2b30848f5816690
Vadim231 <vvvvadim2000@mail.ru>
2018-10-11 09:44:34 +0600

    123

---
+++ MinerCraft/Crafting.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerCraft
+{
+    class Crafting
+    {
+        private string name;
+        //private int weight;
+        //private int hardness;
+
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string str)
+        {
+            name = str;
+        }
+      
+        public static Crafting operator+(Crafting firstOper, Crafting secondOper)
+        {
+            //Кирка
+            if (firstOper.name == "Material" && secondOper.name == "Material")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XX");
+                return striing;
+            }
+            else if (firstOper.name == "XX" && secondOper.name == "Material")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXX");
+                return striing;
+            }
+            else if (firstOper.name == "XXX" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXXO");
+                return striing;
+            }
+            else if (firstOper.name == "XXXO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXXOI");
+                return striing;
+            }
+            else if (firstOper.name == "XXXOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXXOIO");
+                return striing;
+            }
+            else if (firstOper.name == "XXXOIO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXXOIOO");
+                return striing;
+            }
+            else if (firstOper.name == "XXXOIOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXXOIOOI");
+                return striing;
+            }
+            else if (firstOper.name == "XXXOIOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("Кирка(pick)");
+                return striing;
+            }
+
+            //лопата <-
+
+            else if (firstOper.name == "Empty" && secondOper.name == "Material")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OX");
+                return striing;
+            }
+            else if (firstOper.name == "OX" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXO");
+                return striing;
+            }
+            else if (firstOper.name == "OXO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOO");
+                return striing;
+            }
+            else if (firstOper.name == "OXOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOI");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOIO");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOIO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOIOO");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOIOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOIOOI");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOIOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("Лопата(shovel)");
+                return striing;
+            }
+
+            //Топор <-
+            else if (firstOper.name == "XX" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXO");
+                return striing;
+            }
+            else if (firstOper.name == "XXO" && secondOper.name == "Material")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOX");
+                return striing;
+            }
+            else if (firstOper.name == "XXOX" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOXI");
+                return striing;
+            }
+            else if (firstOper.name == "XXOXI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOXIO");
+                return striing;
+            }
+            else if (firstOper.name == "XXOXIO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOXIOO");
+                return striing;
+            }
+            else if (firstOper.name == "XXOXIOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOXIOOI");
+                return striing;
+            }
+            else if (firstOper.name == "XXOXIOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("Топор(Axe)");
+                return striing;
+            }
+
+            //меч
+            else if (firstOper.name == "OXOO" && secondOper.name == "Material")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOX");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOX" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOXO");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOXO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOXOO");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOXOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("OXOOXOOI");
+                return striing;
+            }
+            else if (firstOper.name == "OXOOXOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("Меч(Sword)");
+                return striing;
+            }
+
+            //матыга
+            
+            else if (firstOper.name == "XXO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOO");
+                return striing;
+            }
+            else if (firstOper.name == "XXOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOOI");
+                return striing;
+            }
+            else if (firstOper.name == "XXOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOOIO");
+                return striing;
+            }
+            else if (firstOper.name == "XXOOIO" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOOIOO");
+                return striing;
+            }
+            else if (firstOper.name == "XXOOIOO" && secondOper.name == "Stick")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("XXOOIOOI");
+                return striing;
+            }
+            else if (firstOper.name == "XXOOIOOI" && secondOper.name == "Empty")
+            {
+                Crafting striing = new Crafting();
+                striing.SetName("Матыга(Hoe)");
+                return striing;
+            }
+            else { return null; }
+        }
+
+
+    }
+}
---
+++ MinerCraft/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Материал, палка и пустая ячейка
+            Crafting Material = new Crafting();
+            Crafting Stick = new Crafting();
+            Crafting Empty1 = new Crafting();
+
+            Material.SetName("Material");
+            Stick.SetName("Stick");
+            Empty1.SetName("Empty");
+
+            Crafting[] Items = new Crafting[9];
+
+            string[] ch=new string[9];
+
+
+            //"Обнуляем"
+            for (int i = 0; i < 9; i++)
+            {
+                ch[i] = " ";
+            }
+
+                Console.WriteLine("1-Материал,2-Палка,3-пустота");
+            for(int i = 0; i < 9; i++)
+            {
+                ch[i]=Console.ReadLine();
+                //1,2,3
+                Console.WriteLine("╔═╦═╦═╗\n║"+ch[0]+ "║" + ch[1] + "║" + ch[2] + "║\n╠═╬═╬═╣\n║" + ch[3] + "║" + ch[4] + "║" + ch[5] + "║\n╠═╬═╬═╣\n║" + ch[6] + "║" + ch[7] + "║" + ch[8] + "║\n╚═╩═╩═╝");
+            }
+            for (int i = 0; i < 9; i++) {
+                if (ch[i] == "1") {
+                    Items[i]=Material;
+                }
+                else if (ch[i] == "2") {
+                    Items[i]=Stick;
+                }
+                else if (ch[i] == "3")
+                {
+                    Items[i]=Empty1;
+                }   
+            }
+
+            //Выводим, что получилось
+
+            Crafting NewObject = new Crafting();
+            //NewObject = Material + Material + Empty + Material + Stick + Empty + Empty + Stick + Empty;
+
+            NewObject = Items[0]+ Items[1]+ Items[2]+ Items[3]+ Items[4]+ Items[5]+ Items[6]+ Items[7]+ Items[8];
+            Console.WriteLine(NewObject.GetName());
+
+            //NewObject = Material + Material + Material + Empty + Stick + Empty + Empty + Stick + Empty;
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
